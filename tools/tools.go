1fd106676f965767308a59bd4edc1ad7870bcc19
Cédric <ctrevisan@paciellogroup.com>
2018-10-06 17:57:45 +0200

    Added KeyGen

--- tools/tools.go
+++ tools/tools.go
@@ -1,9 +1,11 @@
 package tools
 
 import (
+	"encoding/hex"
 	"regexp"
 
 	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/scrypt"
 )
 
 const xaalAddrPattern = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"
@@ -24,4 +26,11 @@ func IsValidAddr(val string) bool {
 func GetRandomUUID() string {
 	u1 := uuid.Must(uuid.NewV1()) // panic on error
 	return u1.String()
+}
+
+// Pass2key : Generates key from passphrase using scrypt
+func Pass2key(passphrase string) string {
+	salt := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} // buffer of zeros (crypto_pwhash_scryptsalsa208sha256_SALTBYTES)
+	key, _ := scrypt.Key([]byte(passphrase), []byte(salt), 16384, 8, 1, 32)
+	return hex.EncodeToString(key)
 }
