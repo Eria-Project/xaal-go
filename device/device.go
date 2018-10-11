85018cd8c823908dc68da9dced2f42e5a19c2fd3
Cédric <ctrevisan@paciellogroup.com>
2018-10-11 18:27:08 +0200

    Updated device management

--- device/device.go
+++ device/device.go
@@ -7,36 +7,46 @@ import (
 	"xaal-go/tools"
 )
 
-// Device : xAAL internal attributes for a device
+// Device : xAAL device
 type Device struct {
 	DevType     string // xaal devtype
 	Address     string // xaal addr
 	alivePeriod int    // time in sec between two alive
+	VendorID    string // vendor ID ie : ACME
+	ProductID   string // product ID
+	Version     string // product release
+	URL         string // product URL
+	Info        string // additionnal info
 
 	/*
 		self.devtype = devtype          # xaal devtype
 		self.address = addr             # xaal addr
-		self.vendor_id = None           # vendor ID ie : ACME
-		self.product_id = None          # product ID
-		self.version = None             # product release
-		self.url = None                 # product URL
-		self.info = None                # additionnal info
 		self.hw_id = None               # hardware info
 		self.group_id = None            # group devices
 		# Some useless attributes, only for compatibility
 		self.bus_addr = config.address
 		self.bus_port = config.port
 		self.hops = config.hops
-		# Unsupported stuffs
-		self.unsupported_attributes = []
-		self.unsupported_methods = []
-		self.unsupported_notifications = []
-		# Default attributes & methods
-		self.__attributes = Attributes()
+	*/
+	// Unsupported stuffs
+	unsupportedAttributes []string
+	//	unsupportedMethods = []
+	//	unsupportedNotifications = []
+
+	// Default attributes & methods
+	attributes []Attribute
+	/*
 		self.methods = {'getAttributes' : self._get_attributes,
 						'getDescription': self._get_description }
 		self.engine = engine
 	*/
+}
+
+// Attribute : xAAL internal attributes
+type Attribute struct {
+	name         string
+	defaultValue interface{}
+	value        interface{}
 }
 
 /*SetDevType : Set the device type */
@@ -69,3 +79,69 @@ func (d *Device) SetAddress(address string) error {
 func (d Device) GetTimeout() int {
 	return 2 * d.alivePeriod
 }
+
+// ---------------- attributes ----------------
+
+// NewAttribute : Create a new attribute
+func (d *Device) NewAttribute(name string, defaultValue interface{}) Attribute {
+	attr := Attribute{
+		name:         name,
+		defaultValue: defaultValue,
+	}
+	d.AddAttribute(&attr)
+	return attr
+}
+
+// AddAttribute : Add the attribute to the device list of attributes
+func (d *Device) AddAttribute(attr *Attribute) {
+	if attr != nil {
+		// TODO attr.device = self
+		d.attributes = append(d.attributes, *attr)
+	}
+}
+
+// AddUnsupportedAttribute : Add the attribute string to the list of unsupported
+func (d *Device) AddUnsupportedAttribute(name string) {
+	d.unsupportedAttributes = append(d.unsupportedAttributes, name)
+	d.DelAttribute(name)
+}
+
+// DelAttribute : Remove the attribute from device list of attributes
+func (d *Device) DelAttribute(name string) error {
+	if name != "" {
+		// Find element index
+		i := d.findAttributeIndex(name)
+		if i > -1 {
+			// Delete index (See https://github.com/golang/go/wiki/SliceTricks)
+			d.attributes = append(d.attributes[:i], d.attributes[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("Attribute not found")
+}
+
+// SetAttributeValue : Set the attribute value
+func (d *Device) SetAttributeValue(name string, value interface{}) {
+	// Search element index
+	i := d.findAttributeIndex(name)
+	if i > -1 {
+		d.attributes[i].value = value
+	}
+}
+
+func (d *Device) findAttributeIndex(name string) int {
+	for i, e := range d.attributes {
+		if e.name == name {
+			return i
+		}
+	}
+	return -1
+}
+
+/*
+def get_attribute(self,name):
+	for attr in self.__attributes:
+		if attr.name == name:
+			return attr
+	return None
+*/
--- device/device_test.go
+++ device/device_test.go
@@ -1,6 +1,7 @@
 package device
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -120,6 +121,283 @@ func TestDevice_GetTimeout(t *testing.T) {
 			}
 			if got := d.GetTimeout(); got != tt.want {
 				t.Errorf("Device.GetTimeout() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDevice_AddAttribute(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		attr *Attribute
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name:   "Integer attribute",
+			fields: fields{},
+			args:   args{attr: &Attribute{"int", 10, 0}},
+		},
+		{
+			name:   "String attribute",
+			fields: fields{},
+			args:   args{attr: &Attribute{"string", "A", "B"}},
+		},
+		{
+			name:   "Bool attribute",
+			fields: fields{},
+			args:   args{attr: &Attribute{"bool", true, false}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			d.AddAttribute(tt.args.attr)
+			if len(d.attributes) == 0 {
+				t.Errorf("TestDevice_AddAttribute The attribute hasn't been added to the list")
+			}
+			if !reflect.DeepEqual(d.attributes[0], *tt.args.attr) {
+				t.Errorf("TestDevice_AddAttribute The attribute doesn't seems to match")
+			}
+		})
+	}
+}
+
+func TestDevice_NewAttribute(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		name         string
+		defaultValue interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   Attribute
+	}{
+		{
+			fields: fields{},
+			args:   args{name: "Test", defaultValue: 0},
+			want:   Attribute{name: "Test", defaultValue: 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			if got := d.NewAttribute(tt.args.name, tt.args.defaultValue); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Device.NewAttribute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDevice_findAttributeIndex(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name:   "Existing attribute",
+			fields: fields{attributes: []Attribute{Attribute{"test", 1, 0}}},
+			args:   args{name: "test"},
+			want:   0,
+		},
+		{
+			name:   "Not existing attribute",
+			fields: fields{},
+			args:   args{name: "test"},
+			want:   -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			if got := d.findAttributeIndex(tt.args.name); got != tt.want {
+				t.Errorf("Device.findAttributeIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDevice_SetAttributeValue(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		name  string
+		value interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			fields: fields{attributes: []Attribute{Attribute{"test", nil, 0}}},
+			args:   args{name: "test", value: 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			d.SetAttributeValue(tt.args.name, tt.args.value)
+			if d.attributes[0].value != tt.args.value {
+				t.Errorf("TestDevice_SetAttributeValue The attribute hasn't been set")
+			}
+		})
+	}
+}
+
+func TestDevice_DelAttribute(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Empty name",
+			fields:  fields{attributes: []Attribute{Attribute{"test", nil, 0}}},
+			args:    args{name: ""},
+			wantErr: true,
+		},
+		{
+			name:    "Not existing name",
+			fields:  fields{attributes: []Attribute{Attribute{"test", nil, 0}}},
+			args:    args{name: "a"},
+			wantErr: true,
+		},
+		{
+			name:    "Existing name",
+			fields:  fields{attributes: []Attribute{Attribute{"test", nil, 0}}},
+			args:    args{name: "test"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			err := d.DelAttribute(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Device.DelAttribute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil && len(d.attributes) != 0 {
+				t.Errorf("Device.DelAttribute() the attribute hasn't been removed from the list")
+			}
+			if err != nil && len(d.attributes) != 1 {
+				t.Errorf("Device.DelAttribute() the attribute has been unexpected removed from the list")
+			}
+		})
+	}
+}
+
+func TestDevice_AddUnsupportedAttribute(t *testing.T) {
+	type fields struct {
+		DevType               string
+		Address               string
+		alivePeriod           int
+		unsupportedAttributes []string
+		attributes            []Attribute
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			fields: fields{},
+			args:   args{name: "test"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Device{
+				DevType:               tt.fields.DevType,
+				Address:               tt.fields.Address,
+				alivePeriod:           tt.fields.alivePeriod,
+				unsupportedAttributes: tt.fields.unsupportedAttributes,
+				attributes:            tt.fields.attributes,
+			}
+			d.AddUnsupportedAttribute(tt.args.name)
+			if len(d.unsupportedAttributes) == 0 {
+				t.Errorf("TestDevice_AddUnsupportedAttribute The attribute hasn't been added to the list")
+			}
+			if d.unsupportedAttributes[0] != tt.args.name {
+				t.Errorf("TestDevice_AddUnsupportedAttribute The attribute doesn't seems to match")
 			}
 		})
 	}
