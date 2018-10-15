45eb916a3a83f746dcadb8ef0c5d8be96d4b7589
Cédric <ctrevisan@paciellogroup.com>
2018-10-15 09:36:00 +0200

    Updated message and added tests

--- message/message.go
+++ message/message.go
@@ -19,16 +19,19 @@ type DataMessage struct {
 
 // Message : xAAL Message struct
 type Message struct {
-	Body   map[string]interface{} `json:"body,omitempty"` // message body
-	Header struct {
-		Source  string `json:"source"`
-		DevType string `json:"devType"`
-		MsgType string `json:"msgType"`
-		Action  string `json:"action"`
-	} `json:"header"` // dict used to store msg headers
-	Targets   []string `json:"-"`
-	Timestamp []int    `json:"-"` // message timestamp
-	Version   string   `json:"-"` // message API version
+	Body      map[string]interface{} `json:"body,omitempty"` // message body
+	Header    Header                 `json:"header"`         // dict used to store msg headers
+	Targets   []string               `json:"-"`
+	Timestamp []int                  `json:"-"` // message timestamp
+	Version   string                 `json:"-"` // message API version
+}
+
+// Header : xAAL Message Header struct
+type Header struct {
+	Source  string `json:"source"`
+	DevType string `json:"devType"`
+	MsgType string `json:"msgType"`
+	Action  string `json:"action"`
 }
 
 // New : Initiate a new Message struct
@@ -65,36 +68,43 @@ func (m Message) Dump() {
 }
 
 /*IsRequest : Return true if the message is a request */
-func (m Message) IsRequest() bool {
+func (m *Message) IsRequest() bool {
 	return m.Header.MsgType == "request"
 }
 
 /*IsReply : Return true if the message is a reply */
-func (m Message) IsReply() bool {
+func (m *Message) IsReply() bool {
 	return m.Header.MsgType == "reply"
 }
 
 /*IsNotify : Return true if the message is a notification */
-func (m Message) IsNotify() bool {
+func (m *Message) IsNotify() bool {
 	return m.Header.MsgType == "notify"
 }
 
 /*IsAlive : Return true if the message is a alive notification */
-func (m Message) IsAlive() bool {
+func (m *Message) IsAlive() bool {
 	return m.Header.MsgType == "notify" && m.Header.Action == "alive"
 }
 
 /*IsAttributesChange : Return true if the message is a attributesChange notification */
-func (m Message) IsAttributesChange() bool {
+func (m *Message) IsAttributesChange() bool {
 	return m.Header.MsgType == "notify" && m.Header.Action == "attributesChange"
 }
 
 /*IsGetAttributeReply : Return true if the message is a getAttributes reply */
-func (m Message) IsGetAttributeReply() bool {
+func (m *Message) IsGetAttributeReply() bool {
 	return m.Header.MsgType == "reply" && m.Header.Action == "getAttributes"
 }
 
 /*IsGetDescriptionReply : Return true if the message is a getDescription reply */
-func (m Message) IsGetDescriptionReply() bool {
+func (m *Message) IsGetDescriptionReply() bool {
 	return m.Header.MsgType == "reply" && m.Header.Action == "getDescription"
+}
+
+// Time : Return timestamp in Time format
+func (m *Message) Time() time.Time {
+	sec := int64(m.Timestamp[0])
+	nsec := int64(m.Timestamp[1] * 1000)
+	return time.Unix(sec, nsec)
 }
---
+++ message/message_test.go
@@ -0,0 +1,267 @@
+// Package message handle the message struct
+package message
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		want Message
+	}{
+		{
+			want: Message{Version: "0.5", Targets: []string{}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsRequest(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "request"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE",
+			fields: fields{Header: Header{MsgType: "xxx"}},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsRequest(); got != tt.want {
+				t.Errorf("Message.IsRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsReply(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "reply"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE",
+			fields: fields{Header: Header{MsgType: "xxx"}},
+			want:   false,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsReply(); got != tt.want {
+				t.Errorf("Message.IsReply() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsNotify(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "notify"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE",
+			fields: fields{Header: Header{MsgType: "xxx"}},
+			want:   false,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsNotify(); got != tt.want {
+				t.Errorf("Message.IsNotify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsAlive(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "notify", Action: "alive"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE type",
+			fields: fields{Header: Header{MsgType: "xxx", Action: "alive"}},
+			want:   false,
+		},
+		{
+			name:   "FALSE action",
+			fields: fields{Header: Header{MsgType: "notify", Action: "xxx"}},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsAlive(); got != tt.want {
+				t.Errorf("Message.IsAlive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsAttributesChange(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "notify", Action: "attributesChange"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE type",
+			fields: fields{Header: Header{MsgType: "xxx", Action: "attributesChange"}},
+			want:   false,
+		},
+		{
+			name:   "FALSE action",
+			fields: fields{Header: Header{MsgType: "notify", Action: "xxx"}},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsAttributesChange(); got != tt.want {
+				t.Errorf("Message.IsAttributesChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsGetAttributeReply(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "reply", Action: "getAttributes"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE type",
+			fields: fields{Header: Header{MsgType: "xxx", Action: "getAttributes"}},
+			want:   false,
+		},
+		{
+			name:   "FALSE action",
+			fields: fields{Header: Header{MsgType: "reply", Action: "xxx"}},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsGetAttributeReply(); got != tt.want {
+				t.Errorf("Message.IsGetAttributeReply() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessage_IsGetDescriptionReply(t *testing.T) {
+	type fields struct {
+		Header Header
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "TRUE",
+			fields: fields{Header: Header{MsgType: "reply", Action: "getDescription"}},
+			want:   true,
+		},
+		{
+			name:   "FALSE type",
+			fields: fields{Header: Header{MsgType: "xxx", Action: "getDescription"}},
+			want:   false,
+		},
+		{
+			name:   "FALSE action",
+			fields: fields{Header: Header{MsgType: "reply", Action: "xxx"}},
+			want:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Message{
+				Header: tt.fields.Header,
+			}
+			if got := m.IsGetDescriptionReply(); got != tt.want {
+				t.Errorf("Message.IsGetDescriptionReply() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
